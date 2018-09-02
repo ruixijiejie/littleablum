bfea808f3dad371566e4dd1309126bbf1df9a91a
SoHyun <1819109014@qq.com>
2018-09-02 13:18:31 +0800

    init my project

---
+++ controller/router.js
@@ -0,0 +1,51 @@
+// 首页
+var file = require('../models/file.js')
+// var fs = require("fs");
+// var path = require("path")
+exports.showIndex = function(req,res,next){
+    //错误的：传统思维，不是node的思维：
+    //res.render("index",{
+    //    "albums" : file.getAllAlbums()
+    //});
+
+    //这就是Node.js的编程思维，就是所有的东西，都是异步的
+    //所以，内层函数，不是return回来东西，而是调用高层函数提供的
+    //回调函数。把数据当做回调函数的参数来使用。
+    file.getAllAlbums(function(err,allAlabums){
+        //err是字符串
+        if(err){
+            next(); //交给下面适合他的中间件
+            return;
+        }
+        res.render("index",{
+            "albums" : allAlabums
+        });
+    })
+}
+// 相册页
+exports.showAlbum = function(req,res,next) {
+    //遍历相册中的所有图片
+    var albumName = req.params.albumName;
+    //具体业务交给model
+    file.getAllImagesByAlbumName(albumName,function(err,imagesArray){
+        if(err){
+            next(); //交给下面的中间件
+            return;
+        }
+        res.render("album",{
+            "albumname" : albumName,
+            "images" : imagesArray
+        });
+    });
+};
+
+//显示上传
+exports.showUp = function(req,res) {
+    //命令file模块（我们自己写的函数）调用getAllAlbums函数
+    //得到所有文件夹名字之后做的事情，写在回调函数里面
+    file.getAllAlbums(function(err,albums) {
+        res.render("up",{
+            albums : albums
+        });
+    });
+};
